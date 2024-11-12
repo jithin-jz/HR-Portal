0eb94bb74f2381922f6d1ee8914ee50aa232c4c3
jithin <jithinjzx@gmail.com>
2024-11-12 11:18:53 +0530

    cs

--- Project/settings.py
+++ Project/settings.py
@@ -91,7 +91,7 @@ USE_I18N = True
 USE_TZ = True  # Enable timezone support
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = "media/"
