adf52972229c2e31f279a79af8515da040b7472e
jithin <jithinjzx@gmail.com>
2025-01-16 11:19:55 +0530

    404

--- Project/settings.py
+++ Project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # Debug mode (don't use in production)
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 # Allowed hosts for production
 ALLOWED_HOSTS = ['.vercel.app']
